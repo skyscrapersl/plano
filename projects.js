21af1fed9574a076b164466612dbe8c191662786
skyscrapersl <146325873+skyscrapersl@users.noreply.github.com>
2023-10-15 09:26:19 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,4 +1,3 @@
 const data1 = ["pngmx", "pngbr", "gskjp", "ccaau", "beiersdorfco", "sinoth", "diageous", "pepsicoes", "sanofies", "diageotw", "beiersdorfin", "kibonbr"];
 const data2 = ["abc", "efg", "hijk"];
-
 export { data1, data2 };
