11d15c17d902d67c43423aa4aa3aacb25f8b4662
skyscrapersl <146325873+skyscrapersl@users.noreply.github.com>
2023-10-15 09:43:18 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,2 +1,2 @@
 const data1 = ["pngmx", "pngbr", "gskjp", "ccaau", "beiersdorfco", "sinoth", "diageous", "pepsicoes", "sanofies", "diageotw", "beiersdorfin"];
-const data2 = ["GSSC_G16304.Dharana", "sanjaya@gssintl.biz"];
+const data2 = ["GSSC_G16304.Dharana", "sanjaya@gssintl.biz", "GSSC_G22395.Malki"];
