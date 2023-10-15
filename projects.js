18911353931154f63cb5434d9dce63e9a46befcc
skyscrapersl <146325873+skyscrapersl@users.noreply.github.com>
2023-10-15 11:39:26 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,2 +1,6 @@
 const data1 = ["pngmx", "pngbr", "gskjp", "ccaau", "beiersdorfco", "sinoth", "diageous", "pepsicoes", "sanofies", "diageotw", "beiersdorfin"];
-const data2 = ["GSSC_G16304.Dharana", "sanjaya@gssintl.biz", "GSSC_G22395.Malki"];
+const data2 = U2FsdGVkX1/u9yzOuoRuqggVCptT/ytkVbuRhUSYlRp+2VguY86r3PzJaB5ZmaYAMP2erBKxjpbrNRWgdRDF9oRd+cRiCH/Izd2XrJVxQfDS2V4PBo92eAZ9YFJItxDS;
+const data3 = ["ONGOING"];
+
+
+
