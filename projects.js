45a5a4568dc9589275b9ade9df1720ea1e2ea435
skyscrapersl <146325873+skyscrapersl@users.noreply.github.com>
2023-10-16 17:46:08 +0530

    Update projects.js
    updated

--- projects.js
+++ projects.js
@@ -1,5 +1,5 @@
 const data1 = ["pngmx", "pngbr", "gskjp", "ccaau", "beiersdorfco", "sinoth", "diageous", "pepsicoes", "sanofies", "diageotw", "beiersdorfin"];
-const data2 = U2FsdGVkX1+7XqlXSQ4U9qfdJQg8+HUHqDRtpKxg/B4pExbLQelWyH7LaV1zswErypk7mRkH7k/YglzXVUAgDePGIob7k+lbwCvwbjDOdrOHNkwX8bqgv0xeKxXaQLC+SXOU2szw77lKtuqRLSRQsw==;
+const data2 = U2FsdGVkX1/IQe3LkhMdr8UhekQamflnsEUDKdkWxeV9CT5YKOQ/5DYrUX2SHofxtyZIcVnomshdBZwU13igt+AEiFrFJ3M3L/zEwea0ET9KrgL7eYjAwWjn1wndUj+xHf/4TA6/5aeV2vuH3EWwMA==;
 const data3 = ["ONGOING"];
 
 
