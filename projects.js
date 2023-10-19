757eb9cbcbfeeb5560a511c59b4c77dd375924a3
skyscrapersl <146325873+skyscrapersl@users.noreply.github.com>
2023-10-19 16:13:54 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,5 +1,6 @@
 const data1 = ["pngmx", "pngbr", "gskjp", "ccaau", "beiersdorfco", "sinoth", "diageous", "pepsicoes", "sanofies", "diageotw", "beiersdorfin"];
 const data2 = U2FsdGVkX1+E9LLLliNyD5r6m+bvGnpeUIpN6N9kntYutK0Rq4C6VdfIdf/tV2z7fp4fQK3K29e/QPqi+W84vBG4E4+xx5IsR/1uAyyHfZ0HCGMPg80DDt23lVD7leJI9cWPnhJabA6YlNj7COph/do6dpwmdAW08z6UuRy3LcU=;
+const data4 = U2FsdGVkX18f6P0x+A9ubEZmG6mVAIfE1UeHsqoyUcsplGuaUzwnEg9ZKkyfCWRUNHimBEO19IFbHwajqzi5Ef0yoPAV9HKAA/dYuOYoVCS6IZZurwM4g5aPEqIBxF9rc4c3ttuGMKCZKsBaqEpvaMTg90EzlSvMJDiFSwrp3ldRDYYjZKpOMZnXwyMvge4KuL9S6J1MxY9+hG19vwcjbPHncQq82pnuJEsr4RlbvhyeuMXqmA+Qdh9+UoxsQRmLfJxLVfizOgxD5REE6J/lzQ==;
 const data3 = ["ONGOING"];
 
 
