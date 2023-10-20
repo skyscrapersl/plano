ecb9bdadf7ae8304614d8258833ed8294662ca13
skyscrapersl <146325873+skyscrapersl@users.noreply.github.com>
2023-10-20 09:36:44 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,6 +1,6 @@
 const data1 = ["pngmx", "pngbr", "gskjp", "ccaau", "beiersdorfco", "sinoth", "diageous", "pepsicoes", "sanofies", "diageotw", "beiersdorfin"];
 const data2 = U2FsdGVkX1+E9LLLliNyD5r6m+bvGnpeUIpN6N9kntYutK0Rq4C6VdfIdf/tV2z7fp4fQK3K29e/QPqi+W84vBG4E4+xx5IsR/1uAyyHfZ0HCGMPg80DDt23lVD7leJI9cWPnhJabA6YlNj7COph/do6dpwmdAW08z6UuRy3LcU=;
-const data4 = U2FsdGVkX18f6P0x+A9ubEZmG6mVAIfE1UeHsqoyUcsplGuaUzwnEg9ZKkyfCWRUNHimBEO19IFbHwajqzi5Ef0yoPAV9HKAA/dYuOYoVCS6IZZurwM4g5aPEqIBxF9rc4c3ttuGMKCZKsBaqEpvaMTg90EzlSvMJDiFSwrp3ldRDYYjZKpOMZnXwyMvge4KuL9S6J1MxY9+hG19vwcjbPHncQq82pnuJEsr4RlbvhyeuMXqmA+Qdh9+UoxsQRmLfJxLVfizOgxD5REE6J/lzQ==;
+const data4 = U2FsdGVkX1/Jjx8V77M5S3ZeHBYmkQMLkH40P1v7Dq2mFbyBdsf9VlaPSHY0ca54NfuvSU2M9IaHeDd5TlbLVnHn05uNvLT6zgp3L+xPdnZd0An/3d5ob6xzb3ks1A7w5ctXBp5RijeMSJNw8TJO+EgSK2crzoPm4RdyIU85nwErK3W/M/h/AK1z0DUeb0OmIrEliu1uSpnxtRTqvUUrMti9eZoPQFJ2mgeemH4w8jdeu3/cZ1KjJL+7Q9y4ORnZKBD4Kf6LRL0ulU5mmxo7eFJRxXEoPpbLS8QfojFCy6l1RCzI02wyIvgeLcGqM9L4;
 const data3 = ["ONGOING"];
 
 
