9219d3e649b38dbfb115fc0716f8f6f33092ed4f
skyscrapersl <146325873+skyscrapersl@users.noreply.github.com>
2023-10-21 14:56:39 +0530

    Update projects.js
    kibonbr uopdated for masking

--- projects.js
+++ projects.js
@@ -1,4 +1,4 @@
-const data1 = ["pngmx", "pngbr", "gskjp", "ccaau", "beiersdorfco", "sinoth", "diageous", "pepsicoes", "sanofies", "diageotw", "beiersdorfin"];
+const data1 = ["pngmx", "pngbr", "gskjp", "ccaau", "beiersdorfco", "sinoth", "diageous", "pepsicoes", "sanofies", "diageotw", "beiersdorfin", "kibonbr"];
 const data2 = U2FsdGVkX1+E9LLLliNyD5r6m+bvGnpeUIpN6N9kntYutK0Rq4C6VdfIdf/tV2z7fp4fQK3K29e/QPqi+W84vBG4E4+xx5IsR/1uAyyHfZ0HCGMPg80DDt23lVD7leJI9cWPnhJabA6YlNj7COph/do6dpwmdAW08z6UuRy3LcU=;
 const data4 = U2FsdGVkX196H42dCkH6mgpqSkzXHZHO04Xo6+vJzRcru2Z1p/c7mt58ltC36j6viGkAu7CmEIHcCdnzoGHNfGyzwZcpNWIokGPq9ZEY65DRuSZ6PAri63wYK3C7vtHEzBG8vS4O95MpbN+eACOahVlBZHFIuW9J5EuHyYsRQDRoOay4PP0tfK1CO0t5TkglguSJDucgwZCyrxK066pWQOiSGv2BR64cuV/UN194WYCjMeH7s05b+06+vGCvXbNbjkIVNy/ZOaIBIu9iSLvJQBMZoQBKX7jwgSCo58KQkYocj0nl9ZAQjJZi+UpSo7M3;
 
